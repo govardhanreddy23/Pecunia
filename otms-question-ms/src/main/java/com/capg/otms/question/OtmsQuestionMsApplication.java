c0a1934229b29890893252be6cd6f6de3accf5f2
91903 <somnathreddy94@gmail.com>
2020-08-12 11:35:42 +0530

    added otms module

---
+++ otms-question-ms/src/main/java/com/capg/otms/question/OtmsQuestionMsApplication.java
@@ -0,0 +1,13 @@
+package com.capg.otms.question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtmsQuestionMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtmsQuestionMsApplication.class, args);
+	}
+
+}
