86657c8f95f41b064d6911b3896bd06789bf4db6
91903 <somnathreddy94@gmail.com>
2020-08-14 12:41:15 +0530

    implemented crud operations for question module

--- otms-question-ms/src/main/java/com/capg/otms/question/model/Question.java
+++ otms-question-ms/src/main/java/com/capg/otms/question/model/Question.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,6 +14,7 @@ import javax.persistence.Table;
 public class Question {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long questionId;
 	@ElementCollection
 	private List<String> questionOptions;
