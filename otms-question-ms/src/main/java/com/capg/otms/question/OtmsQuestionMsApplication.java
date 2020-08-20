102abe615704ee1064165dcdb5a7e79f7e25b954
GOVARDHAN REDDY <GOVARDHAN REDDY@LAPTOP-ECMEDBUB.mshome.net>
2020-08-20 20:31:16 +0530

    Implemented Eureka server on all modules

--- otms-question-ms/src/main/java/com/capg/otms/question/OtmsQuestionMsApplication.java
+++ otms-question-ms/src/main/java/com/capg/otms/question/OtmsQuestionMsApplication.java
@@ -2,8 +2,10 @@ package com.capg.otms.question;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OtmsQuestionMsApplication {
 
 	public static void main(String[] args) {
--- otms-user-ms/src/main/java/com/capg/otms/user/OtmsUserMsApplication.java
+++ otms-user-ms/src/main/java/com/capg/otms/user/OtmsUserMsApplication.java
@@ -2,10 +2,13 @@ package com.capg.otms.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OtmsUserMsApplication {
 	
 
