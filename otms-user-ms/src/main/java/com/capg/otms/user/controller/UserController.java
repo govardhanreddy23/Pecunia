35e3587b3688e6860229950c53ec79311956123c
GOVARDHAN REDDY <GOVARDHAN REDDY@This-Pc>
2020-08-27 01:09:09 +0530

    user controller

--- otms-user-ms/src/main/java/com/capg/otms/user/controller/UserController.java
+++ otms-user-ms/src/main/java/com/capg/otms/user/controller/UserController.java
@@ -3,14 +3,11 @@ package com.capg.otms.user.controller;
 
 	import java.net.URISyntaxException;
 	import java.util.List;
-	import java.util.Set;
-
-	import javax.annotation.PostConstruct;
-
 	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.http.HttpStatus;
 	import org.springframework.http.ResponseEntity;
-	import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.PathVariable;
 	import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +22,7 @@ package com.capg.otms.user.controller;
 	import com.capg.otms.user.service.IUserService;
 
 	@RestController
+	@CrossOrigin(origins = "http://localhost:4200")
 	public class UserController {
 		@Autowired
 		IUserService service;
