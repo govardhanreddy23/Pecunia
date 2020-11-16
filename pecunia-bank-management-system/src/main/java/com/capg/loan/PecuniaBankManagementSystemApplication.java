4d2b1045968321f260ec60e9a104d5393eee9636
GOVARDHAN REDDY <GOVARDHAN REDDY@This-Pc>
2020-11-16 11:27:05 +0530

    structure

---
+++ pecunia-bank-management-system/src/main/java/com/capg/loan/PecuniaBankManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.capg.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecuniaBankManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecuniaBankManagementSystemApplication.class, args);
+	}
+
+}
